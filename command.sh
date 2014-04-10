273f4addaf52c8bb425881e66bdcd4cdc9235fbf
silver <silver@silvermag.net>
2014-04-10 01:25:46 -0700

    removed stupid extra script

--- command.sh
+++ command.sh
@@ -7,7 +7,9 @@ case $ACTION in
   echo "clicking"
   aplay beep.wav
   adintool -in mic -out file -filename file -oneshot
-  ./gspeech.sh
+  rm file.flac  > /dev/null 2>&1
+  ffmpeg -i file.wav -ar 16000 -acodec flac file.flac
+  wget -q -U "Mozilla/5.0" --post-file file.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  >stt.txt
   playsome $(cat stt.txt | awk '{print tolower($0)}' | tr -d ' ') &
   ;;
 "COMP STOP")
