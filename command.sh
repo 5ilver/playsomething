660f2c8553707b545795b307d65ecb93bf3e002e
silver <silver@silvermag.net>
2014-04-10 02:31:19 -0700

    added flite and tabs

--- command.sh
+++ command.sh
@@ -11,20 +11,26 @@ fi;
 while listen; do 
 ACTION=`julius -quiet -input rawfile -filelist files -C sample.jconf | grep sentence1: | sed -e 's/sentence1: <s> \(.*\) <\/s>/\1/'`
 case $ACTION in
-"COMP PLAY")
-  echo "clicking"
-  aplay beep.wav
-  listen
-  rm file.flac  > /dev/null 2>&1
-  $encoder -i file.wav -ar 16000 -acodec flac file.flac
-  wget -q -U "Mozilla/5.0" --post-file file.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  >stt.txt
-  vlc $(curl http://www.reddit.com/r/$(cat stt.txt | awk '{print tolower($0)}' | tr -d ' ')/  | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep "http://www.youtube\|https://www.youtube" | sed 's/https:/http:/g' ) &
-  ;;
-"COMP STOP")
-  pkill vlc
-  ;;
+	"COMP PLAY")
+		flite -t "What should I play?" 
+		listen
+		rm file.flac  > /dev/null 2>&1
+		$encoder -i file.wav -ar 16000 -acodec flac file.flac
+		wget -q -U "Mozilla/5.0" --post-file file.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  >stt.txt
+		playlist="$(curl http://www.reddit.com/r/$(cat stt.txt | awk '{print tolower($0)}' | tr -d ' ')/  | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep "http://www.youtube\|https://www.youtube" | sed 's/https:/http:/g' )" 
+ 		if [ "$playlist" ]; then
+  			flite -t "Playing youtube links from R slash $(cat stt.txt)" 
+  			printf "$playlist" | vlc - &
+		else
+			flite -t "No links found"
+		fi
+		;;
+	"COMP STOP")
+		flite -t "Killing VLC instances"
+		pkill vlc
+		;;
 *)
-  echo "wtf..."
-  ;;
+		echo "wtf..."
+		;;
 esac
 done;
