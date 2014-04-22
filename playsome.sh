fe2e8c30cf137b357e4ee317805c2a60271fcacf
silver <a@b.c>
2014-04-22 16:01:04 -0700

    added new playome.sh tweaks

--- playsome.sh
+++ playsome.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-playlist="$(curl http://www.reddit.com/r/$1/  | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep "http://www.youtube\|https://www.youtube" | sed 's/https:/http:/g' | sort | uniq | sort -R )" 
+mkdir ~/.playsome
+cd ~/.playsome
+playlist="$(curl http://www.reddit.com/r/$1/  | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep "http://www.youtube\|https://www.youtube\|http://soundcloud.com\|http://www.soundcloud.com" | sed 's/https:/http:/g' | sort | uniq | sort -R )" 
 if [ "$playlist" ]; then
-	#printf $playlist
-	playsong="$(printf $playlist | head -1)"
-	#echo $playsong
-	playfile="$(youtube-dl -x --get-filename $playsong)"
-	#echo $playfile
-	youtube-dl -q -x -w "$playsong"
-	cvlc -q "$playfile" "vlc://quit"
+	echo "$playlist"
+	echo "$playlist" | while read playsong; do 
+		echo $playsong
+		playfile="$(youtube-dl -x --get-filename $playsong)"
+		echo $playfile
+		youtube-dl -q -x -w "$playsong"
+		cvlc "$playfile" "vlc://quit"
+	done
 else
-	exit 2
+	echo "No links found"
 fi
